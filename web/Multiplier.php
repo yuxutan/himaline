07b258111d79e54183b10ca247fc6eeba920dfee
yuxutan <yuxutan@gmail.com>
2016-06-09 09:26:19 +0900

    redis & php-resque connect test

---
+++ web/Multiplier.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuxut
+ * Date: 2016/06/09
+ * Time: 9:22
+ */
+class Multiplier
+{
+
+    public function setUp()
+    {
+    }
+
+    public function perform()
+    {
+        $x = $this->args['x'];
+        $y = $this->args['y'];
+        sleep(1); // テストのため重い処理っぽい雰囲気を出す
+        $result = $x * $y;
+        echo "Multiply: x * y = $result" . PHP_EOL;
+    }
+
+    public function tearDown()
+    {
+    }
+
+}
