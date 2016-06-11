08da136971382c4df1b8fa8ece5ad84accfdc590
yuxutan <sakt1128>
2016-06-11 13:30:32 +0900

    use php-resque

--- web/callback.php
+++ web/callback.php
@@ -3,9 +3,9 @@ error_log("==========callback start.====================");
 
 // enqueue.php
 require '../vendor/autoload.php';
-Resque::setBackend('localhost:6379');
-$args = array('x' => 12, 'y' => 31);
-Resque::enqueue('multiply', 'Multiplier', $args);
+//Resque::setBackend('localhost:6379');
+//$args = array('x' => 12, 'y' => 31);
+//Resque::enqueue('multiply', 'Multiplier', $args);
 
 
 
